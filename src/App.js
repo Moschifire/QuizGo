a2808a27fd21d031391bece2992148ffc287e13c
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2024-03-17 07:19:17 +0100

    join quiz screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import JoinScreen from './components/JoinScreen';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="quiz-container">
+        <JoinScreen />
+      </div>
     </>
   );
 }
